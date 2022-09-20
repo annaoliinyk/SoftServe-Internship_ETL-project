682b54f7fb79b3048d19472f9c26f366c17a6ad2
annaoliinyk <anna12ol345@gmail.com>
2022-09-20 22:37:20 +0200

    Added authentication while accessing states to get unlimited results

--- extract/get_states.py
+++ extract/get_states.py
@@ -1,12 +1,28 @@
+import logging
+import sys
+
 # used "pip install -e "git+https://github.com/openskynetwork/opensky-api.git#egg=opensky-api&subdirectory=python"
 # command to install api from GitHub
 from src.python.python.opensky_api import OpenSkyApi
 
-
-def get_states():
-    api = OpenSkyApi()
-    s = api.get_states()
-    print(s)
+# want to see if connection is successful:
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-get_states()
+def get_credentials_from_file():
+    file = open("opensky_credentials", "r").read()
+    # get username, password from first and second lines of text file:
+    username, password = file.split("\n")[0], file.split("\n")[1]
+    return username, password
+
+
+def main():
+    username, password = get_credentials_from_file()
+    api = OpenSkyApi(username=username, password=password)
+    states = api.get_states()
+    for s in states.states:
+        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+
+
+if __name__ == "__main__":
+    main()
