db61c1c0dd83610f64dc110a330c8146257b51c6
annaoliinyk <anna12ol345@gmail.com>
2022-09-21 16:50:49 +0200

    Moved opensky_credentials to a single folder, refactored

--- extract_data_from_OpenSkyApi/get_states.py
+++ extract_data_from_OpenSkyApi/get_states.py
@@ -9,7 +9,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 def get_credentials_from_file():
-    file = open(r"extract_data_from_OpenSkyApi\opensky_credentials", "r").read()
+    file = open(r"configs\opensky_credentials", "r").read()
     # get username, password from first and second lines of text file:
     username, password = file.split("\n")[0], file.split("\n")[1]
     return {"login": username, "password": password}
