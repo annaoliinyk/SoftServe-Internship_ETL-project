38de88029cbdf22228a57c07ff9270653aa80533
annaoliinyk <anna12ol345@gmail.com>
2022-09-21 16:41:33 +0200

    Beautified print

--- extract_data_from_OpenSkyApi/__init__.py
+++ extract_data_from_OpenSkyApi/__init__.py
@@ -1,7 +1,8 @@
+import pprint
+
 import extract_data_from_OpenSkyApi.get_states
 
 
 def print_states():
     states = get_states.get_states()
-    for s in states.states:
-        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
+    pprint.pprint(states)
