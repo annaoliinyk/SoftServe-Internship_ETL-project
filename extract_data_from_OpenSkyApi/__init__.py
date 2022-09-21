9bb7ba01107e07e58a257d840925821f9db10742
annaoliinyk <anna12ol345@gmail.com>
2022-09-21 15:52:16 +0200

    Moved print_states() method to a single file

--- extract_data_from_OpenSkyApi/__init__.py
+++ extract_data_from_OpenSkyApi/__init__.py
@@ -1,1 +1,7 @@
-import get_states
+import extract_data_from_OpenSkyApi.get_states
+
+
+def print_states():
+    states = get_states.get_states()
+    for s in states.states:
+        print("(%r, %r, %r, %r)" % (s.longitude, s.latitude, s.baro_altitude, s.velocity))
