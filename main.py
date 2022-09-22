3d065a846716a07747ee625c8ce12e13fa801999
annaoliinyk <anna12ol345@gmail.com>
2022-09-22 13:09:11 +0200

    Added logging info about timestamp to main()

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import extract_data_from_OpenSkyApi as extract
+import logging
+import sys
+from datetime import datetime
+
+from extract_data_from_OpenSkyApi.get_states import DataIngestion
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 def main():
-    extract.print_states()
+    start_timestamp = datetime.now().timestamp()
+    logging.info("Program started: {}".format(start_timestamp))
+    DataIngestion.print_states()
+    end_timestamp = datetime.now().timestamp()
+    logging.info("Program ended: {}".format(end_timestamp))
+    logging.info("Program running took: {}".format(end_timestamp - start_timestamp))
 
 
 if __name__ == "__main__":
