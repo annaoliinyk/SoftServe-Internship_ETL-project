69ee6ebdcb5a57a1aaba6c936eddbc5276aa1eac
annaoliinyk <anna12ol345@gmail.com>
2022-09-26 11:44:07 +0200

    Adding dag for printing states

---
+++ airflow_docker/dags/create_data_extraction_dag.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from OpenSkyDataExtractor.get_states import DataIngestion
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
+    'start_day': days_ago(0, 0, 0, 0, 0)
+}
+
+
+def print_states():
+    DataIngestion().print_states()
+
+
+with DAG(
+        dag_id='ingest_data_from_OpenSkyApi_v1',
+        default_args=DEFAULT_ARGS,
+        description='This is a dag that logs into https://opensky-network.org/api/states/all, and then prints all '
+                    'the states',
+        start_date=days_ago(0, 0, 0, 0, 0),
+) as dag:
+    extract_data = PythonOperator(
+        task_id='print_states',
+        python_callable=print_states
+    )
+
+extract_data
