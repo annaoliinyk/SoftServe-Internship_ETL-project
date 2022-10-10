5dacd63e93dd17c50dac77bc13619aa647093700
annaoliinyk <anna12ol345@gmail.com>
2022-10-10 16:50:24 +0200

    Added a consumer file

---
+++ kafka/consumer.py
@@ -0,0 +1,11 @@
+import json
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    consumer = KafkaConsumer(
+        'OpenSky_data_ingestion',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='earliest'
+    )
+    for state in consumer:
+        print(json.loads(state.value))
