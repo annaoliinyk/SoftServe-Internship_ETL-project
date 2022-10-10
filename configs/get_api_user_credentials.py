ff5cca547fe63afef3133187bdd0ff3b2e244b4a
annaoliinyk <anna12ol345@gmail.com>
2022-10-10 12:32:48 +0200

    Added file path instead of just file name

--- configs/get_api_user_credentials.py
+++ configs/get_api_user_credentials.py
@@ -1,8 +1,12 @@
 import json
 
+import definitions
+
+CONFIG_PATH = definitions.CONFIG_PATH
+
 
 def get_credentials_from_file():
-    with open(r"opensky_credentials.json") as json_file:
+    with open(CONFIG_PATH + r"\opensky_credentials.json") as json_file:
         data = json.load(json_file)
         # get username, password from first and second lines of text file:
         username, password = data["username"], data["password"]
