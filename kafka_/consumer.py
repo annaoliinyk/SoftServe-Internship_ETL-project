03a255e33483d20b1a0279cd2bc694dd95d0d53a
annaoliinyk <anna12ol345@gmail.com>
2022-10-17 12:04:47 +0200

    Refacted code to use classes

--- kafka_/consumer.py
+++ kafka_/consumer.py
@@ -3,11 +3,14 @@ import json
 from kafka import KafkaConsumer
 
 
-def run_consumer():
-    consumer = KafkaConsumer(
-        'OpenSky_data_ingestion',
-        bootstrap_servers='localhost:9092',
-        auto_offset_reset='earliest'
-    )
-    for state in consumer:
-        print(json.loads(state.value))
+class MyConsumer:
+    def __init__(self):
+        self.consumer = KafkaConsumer(
+            'OpenSky_data_ingestion',
+            bootstrap_servers='localhost:9092',
+            auto_offset_reset='earliest'
+        )
+
+    def run_consumer(self):
+        for state in self.consumer:
+            print(json.loads(state.value))
--- kafka_/producer.py
+++ kafka_/producer.py
@@ -5,25 +5,25 @@ from datetime import datetime
 
 from kafka import KafkaProducer
 
-from get_states_OpenSky import get_a_state
+from kafka_.get_states_OpenSky import get_a_state
 
 
-# Messages will be serialized as json
-def serializer(state):
-    return json.dumps(state).encode('utf-8')
+class MyProducer:
+    def __init__(self):
+        # Kafka Producer
+        self.producer = KafkaProducer(
+            bootstrap_servers=['localhost:9092'],
+            value_serializer=self.serializer
+        )
 
+    # Messages will be serialized as json
+    def serializer(self, state):
+        return json.dumps(state).encode('utf-8')
 
-# Kafka Producer
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=serializer
-)
-
-
-def run_producer():
-    while True:
-        state = get_a_state()
-        print(f'Getting a state @{datetime.now()} | State info = {str(get_a_state())}')
-        producer.send('OpenSky_data_ingestion', state)
-        sleep_time = random.randint(1, 11)
-        time.sleep(sleep_time)
+    def run_producer(self):
+        while True:
+            state = get_a_state()
+            print(f'Getting a state @{datetime.now()} | State info = {str(get_a_state())}')
+            self.producer.send('OpenSky_data_ingestion', state)
+            sleep_time = random.randint(1, 11)
+            time.sleep(sleep_time)
