dc073fe08ea8ce47fa316e90e2f28ad723f7d6c4
annaoliinyk <anna12ol345@gmail.com>
2022-10-27 13:24:00 +0200

    Splitted into two methods

--- kafka_messenger/get_states_OpenSky.py
+++ kafka_messenger/get_states_OpenSky.py
@@ -1,14 +1,17 @@
 from configs.config import STATE_DICT_KEYS
-from kafka_messenger.data_editor import RandomStateExtractor
+from kafka_messenger.data_editor import StatesExtractor
 
 
-class RandomState:
-    def get_random_state(self, data_ingestion_obj) -> dict:
+class StatesOpenSky:
+    def get_all_states(self, data_ingestion_obj) -> dict:
         # returns a dict with keys 'states' and 'time', many states and one time value:
         states_dict = data_ingestion_obj.get_states()
-        random_state = RandomStateExtractor().extract_random_state(states_dict)
-        icao24 = random_state[0]  # unique ICAO 24-bit address of the transponder in hex string representation, like id
+        all_states = StatesExtractor().get_all_states(states_dict)
+        return all_states
+
+    def return_state_info_dict(self, state):
+        icao24 = state[0]  # unique ICAO 24-bit address of the transponder in hex string representation, like id
         return {
             STATE_DICT_KEYS[0]: icao24,  # id
-            STATE_DICT_KEYS[1]: random_state  # full info about the state
+            STATE_DICT_KEYS[1]: state  # full info about the state
         }
