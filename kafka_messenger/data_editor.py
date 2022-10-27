c2a28577de0f0d400352eb78e5264274d26f0fbb
annaoliinyk <anna12ol345@gmail.com>
2022-10-27 13:20:48 +0200

    Simplified method to take all states

--- kafka_messenger/data_editor.py
+++ kafka_messenger/data_editor.py
@@ -1,10 +1,4 @@
-from random import randint
-
-
-class RandomStateExtractor:
-    def extract_random_state(self, states_dict):
+class StatesExtractor:
+    def get_all_states(self, states_dict):
         states_only_list = states_dict['states']  # want to get states only in a form of list
-        maximal_index = len(states_only_list) - 1
-        random_int = randint(0, maximal_index)
-        random_state = states_only_list[random_int]
-        return random_state
+        return states_only_list
