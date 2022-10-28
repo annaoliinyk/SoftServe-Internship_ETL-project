ef73fa55e8dadfe8081a66b27b478c51c2677aa2
annaoliinyk <anna12ol345@gmail.com>
2022-10-28 12:50:22 +0200

    Added explanation of code

--- kafka_messenger/consumer.py
+++ kafka_messenger/consumer.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import sys
 
@@ -9,9 +10,16 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 class MyConsumer:
-    def __init__(self):
-        self.consumer = KafkaConsumer(
-            'OpenSky_data_ingestion',
-            bootstrap_servers=BOOTSTRAP_SERVER,
-            auto_offset_reset='earliest'
-        )
+    # link to documentation to KafkaConsumer: https://kafka-python.readthedocs.io/en/master/apidoc/KafkaConsumer.html
+    consumer = KafkaConsumer(
+        'OpenSky_data_ingestion',  # topic name
+        bootstrap_servers=BOOTSTRAP_SERVER,  # default port is 9092
+        auto_offset_reset='earliest'  # a policy for resetting offsets on OffsetOutOfRange errors, default is 'latest'
+    )
+
+    def run_consumer(self):
+        # state is a single chunk of data in topic
+        for state in MyConsumer.consumer:
+            # state is an object of a ConsumerRecord class (kafka.consumer.fetcher.ConsumerRecord),
+            # I want to get information about state, so using state.value:
+            logging.info(json.loads(state.value))
