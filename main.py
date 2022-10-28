83205acaf03900ab12653ffc87a3adcf74f5217f
annaoliinyk <anna12ol345@gmail.com>
2022-10-28 11:20:06 +0200

    moved info about program running time at the end of main method, passing all_states dictionary to producer now

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 import sys
 from datetime import datetime
+
 import kafka.errors
+
 from OpenSkyDataExtractor.get_states import DataIngestion
 from kafka_messenger import producer, consumer
 
@@ -12,15 +14,17 @@ def main():
     start_timestamp = datetime.now().timestamp()
     logging.info("Program started: {}".format(start_timestamp))
     data_ingestion_obj = DataIngestion()
-    data_ingestion_obj.print_states()
+    data_ingestion_obj.print_states()  # just prints states without writing their values
+    all_states = data_ingestion_obj.get_states()  # get all states as a dictionary and assign to variable
+    try:
+        producer.MyProducer().send_all_states(all_states)
+        consumer.MyConsumer().run_consumer()
+    # this error occurs when Docker containers are not running
+    except kafka.errors.NoBrokersAvailable:
+        logging.error("Please execute container before running container and producer")
     end_timestamp = datetime.now().timestamp()
     logging.info("Program ended: {}".format(end_timestamp))
     logging.info("Program running took: {}".format(end_timestamp - start_timestamp))
-    try:
-        consumer.MyConsumer()
-        producer.MyProducer().send_all_states(data_ingestion_obj)
-    except kafka.errors.NoBrokersAvailable:
-        logging.error("Please execute container before running container and producer")
 
 
 if __name__ == "__main__":
