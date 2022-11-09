3eb131da3fa1d796960a700f44549979fc9af628
annaoliinyk <anna12ol345@gmail.com>
2022-11-09 16:44:38 +0100

    Moved airflow docker files to airflow folder

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
-
-# Global defaults and back-compat
-: "${AIRFLOW_HOME:="/usr/local/airflow"}"
-# Fernet key used for password encryption, like "secret key" authenticated cryptography.
-# Using script to solve the frequent Fernet Key cryptography error:
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Local}Executor}"
-
-export \
-  AIRFLOW_HOME \
-  AIRFLOW__CORE__EXECUTOR \
-  AIRFLOW__CORE__FERNET_KEY \
+echo "start"
 
 if [ -e "/requirements.txt" ]; then
+    $(command -v pip) install --user --upgrade pip
     $(command -v pip) install --user -r /requirements.txt
 fi
+
+: "${AIRFLOW_HOME:="/usr/local/airflow"}"
+Executor='LocalExecutor'
+Fernet_key=''
+
+export AIRFLOW_HOME
+export AIRFLOW__CORE__EXECUTOR=$Executor
+export AIRFLOW__CORE__FERNET_KEY=$Fernet_key
+
+echo "AIRFLOW_HOME is $AIRFLOW_HOME"
+echo "AIRFLOW__CORE__EXECUTOR is $AIRFLOW__CORE__EXECUTOR"
+echo "AIRFLOW__CORE__FERNET_KEY is $AIRFLOW__CORE__FERNET_KEY"
 
 if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
     : "${POSTGRES_HOST:="postgres"}"
@@ -28,13 +30,22 @@ fi
 AIRFLOW__DATABASE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
 export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN
 
+echo "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN is $AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"
+
 case "$1" in
   webserver)
+    echo "Webserver started"
     airflow db init
+    echo "Database initialized"
     airflow users  create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
+    echo "User admin created"
     if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
       airflow scheduler &
+      echo "Started scheduler"
     fi
     exec airflow webserver
+    echo "Webserver executed"
     ;;
 esac
+
+read -rn1
