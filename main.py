f4e4380ece0462ca87c80421d3ded3eab3c5d5f3
annaoliinyk <anna12ol345@gmail.com>
2022-11-25 12:57:24 +0100

    Added comments to main.py

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 def main():
     start_timestamp = datetime.now().timestamp()
     logging.info("Program started: {}".format(start_timestamp))
+    # Extract - Ingest data from OpenSky Api
     data_ingestion_obj = DataIngestion()
     data_ingestion_obj.print_states()  # just prints states without writing their values
     all_states = data_ingestion_obj.get_states()  # get all states as a dictionary and assign to variable
+    # try to connect kafka and send single state as a message
     try:
         producer.MyProducer().send_all_states(all_states)
         consumer.MyConsumer().run_consumer()
     # this error occurs when Docker containers are not running
     except kafka.errors.NoBrokersAvailable:
         logging.error("Please execute container before running container and producer")
+    # measure time it took to run the program
     end_timestamp = datetime.now().timestamp()
     logging.info("Program ended: {}".format(end_timestamp))
     logging.info("Program running took: {}".format(end_timestamp - start_timestamp))
