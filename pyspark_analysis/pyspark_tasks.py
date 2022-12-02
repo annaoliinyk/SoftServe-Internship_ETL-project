c4690a4f1e14b779ceefee27aedb84447f80ef6d
annaoliinyk <anna12ol345@gmail.com>
2022-12-02 11:16:02 +0100

    Moved constants to config.py

--- pyspark_analysis/pyspark_tasks.py
+++ pyspark_analysis/pyspark_tasks.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import max, min, sum
 from pyspark.sql.types import *
 
-from config import JSON_PATH, france_neighbours
+from config import *
 
 SPARK = SparkSession.builder.appName("SparkSQL").getOrCreate()
 DF_SCHEMA = StructType([StructField("icao24", StringType(), True), StructField("callsign", StringType(), True),
@@ -35,25 +35,35 @@ def show_df(df: DataFrame):
 
 def get_highest_altitude(df: DataFrame):
     logging.info("Airplane(s) with highest geo altitude:")
-    max_altitude_df = df.select([max("geo_altitude")])
-    max_altitude = max_altitude_df.first()[0]
-    max_altitude_df2 = df.select(df["icao24"], df["geo_altitude"]).filter(df["geo_altitude"] == max_altitude)
-    show_df(max_altitude_df2)
-    return max_altitude_df2
+    max_altitude_df = get_highest_of_column(df, column_id, column_altitude)
+    show_df(max_altitude_df)
+    return max_altitude_df
 
 
 def get_highest_velocity(df: DataFrame):
     logging.info("Airplane(s) with highest velocity:")
-    max_velocity_df = df.select([max("velocity")])
-    max_velocity = max_velocity_df.first()[0]
-    max_velocity_df2 = df.select(df["icao24"], df["velocity"]).filter(df["velocity"] == max_velocity)
-    show_df(max_velocity_df2)
-    return max_velocity_df2
+    max_velocity_df = get_highest_of_column(df, column_id, column_velocity)
+    show_df(max_velocity_df)
+    return max_velocity_df
+
+
+def get_highest_of_column(df, column1, column2):
+    max_column2_value_df = df.select([max(column2)])
+    max_column2_value = max_column2_value_df.first()[0]
+    result_df = df.select(df[column1], df[column2]).filter(df[column2] == max_column2_value)
+    return result_df
+
+
+def get_minimum_of_column(df, column1, column2):
+    max_column2_value_df = df.select([min(column2)])
+    max_column2_value = max_column2_value_df.first()[0]
+    result_df = df.select(df[column1], df[column2]).filter(df[column2] == max_column2_value)
+    return result_df
 
 
 def get_airplanes_count_by_airport(df: DataFrame):
     logging.info("Count of airplanes by airport:")
-    airplanes_count_df = df.groupBy("origin_country").count()
+    airplanes_count_df = df.groupBy(column_country).count()
     show_df(airplanes_count_df)
     get_country_min_count_airplanes(airplanes_count_df)
     get_country_max_count_airplanes(airplanes_count_df)
@@ -65,46 +75,44 @@ def get_airplanes_count_by_airport(df: DataFrame):
 
 def get_country_min_count_airplanes(airplanes_df: DataFrame):
     logging.info("Countries with smallest number of airplanes")
-    min_count_airplanes_df = airplanes_df.select([min("count")])
-    min_count_airplanes = min_count_airplanes_df.first()[0]
-    min_count_airplanes_df2 = airplanes_df.select(airplanes_df["origin_country"], airplanes_df["count"]).filter(
-        airplanes_df["count"] == min_count_airplanes)
-    show_df(min_count_airplanes_df2)
-    return min_count_airplanes_df2
+    min_count_airplanes_df = get_minimum_of_column(airplanes_df, column_country, column_count)
+    show_df(min_count_airplanes_df)
+    return min_count_airplanes_df
 
 
 def get_country_max_count_airplanes(airplanes_df: DataFrame):
     logging.info("Countries with largest number of airplanes")
-    max_count_airplanes_df = airplanes_df.select([max("count")])
-    max_count_airplanes = max_count_airplanes_df.first()[0]
-    max_count_airplanes_df2 = airplanes_df.select(airplanes_df["origin_country"], airplanes_df["count"]).filter(
-        airplanes_df["count"] == max_count_airplanes)
-    show_df(max_count_airplanes_df2)
-    return max_count_airplanes_df2
+    max_count_airplanes_df = get_highest_of_column(airplanes_df, column_country, column_count)
+    show_df(max_count_airplanes_df)
+    return max_count_airplanes_df
 
 
 def get_count_for_countries_on_c(airplanes_df: DataFrame):
     logging.info("Sum of airplanes for countries starting with C")
-    countries_on_c_df = airplanes_df.filter(airplanes_df.origin_country.startswith("C"))
-    sum_airplanes_c_df = countries_on_c_df.select(
-        [sum("count").alias("sum of airplanes from countries starting with C")])
+    sum_airplanes_c_df = get_count_for_countries(airplanes_df, "C")
     show_df(sum_airplanes_c_df)
     return sum_airplanes_c_df
 
 
 def get_count_for_countries_on_g(airplanes_df: DataFrame):
     logging.info("Sum of airplanes for countries starting with G")
-    countries_on_g_df = airplanes_df.filter(airplanes_df.origin_country.startswith("G"))
-    sum_airplanes_g_df = countries_on_g_df.select(
-        [sum("count").alias("sum of airplanes from countries starting with G")])
+    sum_airplanes_g_df = get_count_for_countries(airplanes_df, "G")
     show_df(sum_airplanes_g_df)
     return sum_airplanes_g_df
+
+
+def get_count_for_countries(df, first_letter: str):
+    countries_on_g_df = df.filter(df.origin_country.startswith(first_letter))
+    sum_airplanes_df = countries_on_g_df.select(
+        [sum(column_count).alias("sum of airplanes from countries starting with " + first_letter)])
+    return sum_airplanes_df
 
 
 def get_count_for_france_neighbours(airplanes_df: DataFrame):
     logging.info("Sum of airplanes of France neighbour countries:")
     france_neighbours_df = airplanes_df.filter(airplanes_df.origin_country.isin(france_neighbours))
-    sum_airplanes_df = france_neighbours_df.select([sum("count").alias("sum of airplanes of neighbours of France")])
+    sum_airplanes_df = france_neighbours_df.select(
+        [sum(column_count).alias("sum of airplanes of neighbours of France")])
     show_df(sum_airplanes_df)
     return sum_airplanes_df
 
