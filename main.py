404b4f4982a7e3d28573ceb35b6f03654d86ddd5
annaoliinyk <anna12ol345@gmail.com>
2022-12-02 12:25:05 +0100

    Added calling spark to main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import kafka.errors
 from kafka_messenger import producer, consumer
 from OpenSkyDataExtractor.get_states import DataIngestion
+from pyspark_analysis.pyspark_tasks import MySparkCalculations
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -23,6 +24,12 @@ def main():
     # this error occurs when Docker containers are not running
     except kafka.errors.NoBrokersAvailable:
         logging.error("Please execute container before running container and producer")
+    # spark:
+    spark_task_obj = MySparkCalculations()
+    spark_task_obj.get_highest_altitude()
+    spark_task_obj.get_highest_velocity()
+    spark_task_obj.get_airplanes_count_by_airport()
+    spark_task_obj.stop_spark()
     # measure time it took to run the program
     end_timestamp = datetime.now().timestamp()
     logging.info("Program ended: {}".format(end_timestamp))
